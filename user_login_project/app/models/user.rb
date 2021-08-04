59800ce7e79032e6a3de8a3e7ee2bec865333c10
jsprofessional <jshuman@protonmail.com>
2021-08-04 17:08:25 -0500

    Finished assignment. Hoping data saved

--- user_login_project/app/models/user.rb
+++ user_login_project/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     validates :first_name, :last_name, :email, :age, presence: true
-
+    validates :first_name, :last_name, length: { minimum: 2 }
+    validates :age, numericality: { greater_than: 9, less_than: 151, only_integer: true }
 end
